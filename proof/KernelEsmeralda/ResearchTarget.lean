52528fbd9dd138c7c494f248318a47c070c11c08
Álvaro Alencar <ac.alvaro@gmail.com>
2026-08-13 20:25:03 -0300

    Define barreira esmeralda

--- proof/KernelEsmeralda/ResearchTarget.lean
+++ proof/KernelEsmeralda/ResearchTarget.lean
@@ -7,5 +7,10 @@ namespace KernelEsmeralda
 
 noncomputable section
 
+def emeraldBarrier (n : Nat) : Real :=
+  Real.log (((n + 1 : Nat) : Real)) +
+    (Real.log 4 + 4) * (upperSquare n ^ (1 / (3 : Real))) +
+    (Real.log 4 + 4) * (upperSquare n ^ (1 / (5 : Real)))
+
 end
 end KernelEsmeralda
