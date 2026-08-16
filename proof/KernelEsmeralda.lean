c0540232878b50c6242d34e68c7f32e8a492b00d
Álvaro Alencar <ac.alvaro@gmail.com>
2026-08-15 23:33:58 -0300

    Add complex Poisson energy bridge

--- proof/KernelEsmeralda.lean
+++ proof/KernelEsmeralda.lean
@@ -69,6 +69,7 @@ import KernelEsmeralda.ResearchFrontier
 import KernelEsmeralda.ComplexPoissonTarget
 import KernelEsmeralda.ImaginaryPhiDepth
 import KernelEsmeralda.ImaginaryPhiDepthQuantitative
+import KernelEsmeralda.ComplexPoissonEnergy
 import KernelEsmeralda.ClaudeTwoThirdsBridge
 import KernelEsmeralda.ClaudeOffLineCount
 import KernelEsmeralda.OffLineBetaBound
