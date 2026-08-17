862716dc9a2425c645a7c05a52b669938afb7a79
Álvaro Alencar <ac.alvaro@gmail.com>
2026-08-17 11:32:58 -0300

    Normalize complex real part in imPart trace

--- proof/KernelEsmeralda/ImPartTrace.lean
+++ proof/KernelEsmeralda/ImPartTrace.lean
@@ -23,10 +23,14 @@ theorem rtrace_imPart_eq_imaginary_energy
   rw [trace_sum, map_sum]
   refine sum_congr rfl fun z hz => ?_
   rw [trace_smul, trace_vecMulVec, smul_eq_mul, dotProduct]
-  simp only [Zeta23.ZeroSide.ZeroBlockData.yv, Complex.mul_re,
-    Complex.ofReal_re, Complex.ofReal_im, mul_zero, sub_zero,
-    Complex.ofReal_natCast]
+  simp only [Zeta23.ZeroSide.ZeroBlockData.yv,
+    Complex.ofReal_re, Complex.ofReal_im, Complex.ofReal_natCast]
   push_cast
+  change Complex.re
+      ((((D.m z : ℝ) : ℂ) *
+        ∑ x, (((D.v z x).im : ℂ) ^ 2)) * 2) =
+    ((D.m z : ℝ) * ∑ x, (D.v z x).im ^ 2) * 2
+  simp
   ring
 
 end KernelEsmeralda
