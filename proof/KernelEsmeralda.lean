c937b98f6245ee36933d6b2a3bef0a744386c940
Álvaro Alencar <ac.alvaro@gmail.com>
2026-08-17 11:28:38 -0300

    Kernel-check imPart trace energy bridge

--- proof/KernelEsmeralda.lean
+++ proof/KernelEsmeralda.lean
@@ -77,6 +77,7 @@ import KernelEsmeralda.ComplexPoissonConcreteEnergy
 import KernelEsmeralda.ComplexPoissonImaginaryEnergy
 import KernelEsmeralda.FiniteEnergyCapture
 import KernelEsmeralda.FiniteImaginaryEnergy
+import KernelEsmeralda.ImPartTrace
 import KernelEsmeralda.GammaOfGeometry
 import KernelEsmeralda.OffLineComplexEnergy
 import KernelEsmeralda.ClaudeTwoThirdsBridge
